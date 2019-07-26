8c92e57fc42bdaa48bf5c75f6bccd99dfa07f0e0
Kostas Kolomeets <kostas.kolomeets@intercom.io>
2019-07-26 16:55:48 +0100

    Add tests for listing component

--- tests/integration/components/rental-listing-test.js
+++ tests/integration/components/rental-listing-test.js
@@ -1,26 +1,40 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | rental-listing', function(hooks) {
+const rental = {
+  image: 'fake.png',
+  title: 'test-title',
+  owner: 'test-owner',
+  type: 'test-type',
+  city: 'test-city',
+  bedrooms: 3
+};
+
+module('Integration | Component | rental-listing', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  hooks.beforeEach(function () {
+    this.rental = rental;
+  });
 
-    await render(hbs`<RentalListing />`);
+  test('should display rental details', async function (assert) {
+    await render(hbs`<RentalListing @rental={{this.rental}} />`);
+    assert.equal(this.element.querySelector('.listing h3').textContent.trim(), rental.title, `Title: ${rental.title}`);
+    assert.equal(this.element.querySelector('.listing .owner').textContent.trim(), `Owner: ${rental.owner}`, `Owner: ${rental.owner}`);
+  });
 
-    assert.equal(this.element.textContent.trim(), '');
+  test('should toggle wide class on click', async function (assert) {
+    const image = () => this.element.querySelector('.image.wide');
 
-    // Template block usage:
-    await render(hbs`
-      <RentalListing>
-        template block text
-      </RentalListing>
-    `);
+    await render(hbs`<RentalListing @rental={{this.rental}} />`);
+    assert.notOk(image(), 'initially rendered small');
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    await click('.image');
+    assert.ok(image(), 'rendered wide after click');
+
+    await click('.image');
+    assert.notOk(image(), 'rendered small after second click');
   });
 });
