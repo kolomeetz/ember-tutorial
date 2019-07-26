bfab02a7667184ab0bee1529808eee0af7366266
Kostas Kolomeets <kostas.kolomeets@intercom.io>
2019-07-26 09:30:26 +0100

    Add navigation tests to rentals page

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, currentURL, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | list rentals', function (hooks) {
@@ -10,12 +10,18 @@ module('Acceptance | list rentals', function (hooks) {
     assert.equal(currentURL(), '/rentals', 'should redirect')
   });
 
-  // test('should link to information about the company', async function (assert) {
-  // });
-  //
-  // test('should link to contact information', async function (assert) {
-  // });
-  //
+  test('should link to information about the company', async function (assert) {
+    await visit('/');
+    await click('.menu-about');
+    assert.equal(currentURL(), '/about', 'should navigate to about')
+  });
+
+  test('should link to contact information', async function (assert) {
+    await visit('/');
+    await click('.menu-contact');
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  });
+
   // test('should list available rentals', async function (assert) {
   // });
   //
