c73ab8177854d444e25461f0528b408ce646d060
Kostas Kolomeets <kostas.kolomeets@intercom.io>
2019-07-26 15:21:52 +0100

    Add toggable image size

--- app/components/rental-listing.js
+++ app/components/rental-listing.js
@@ -1,4 +1,12 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  isWide: false,
+
+  actions: {
+    toggleImageSize() {
+      console.log('toggled');
+      this.toggleProperty('isWide');
+    }
+  }
 });
